772db921a12a2441ffe91a1822e4cf4f4ca40987
Rami <ramiferjani31@gmail.com>
2021-03-25 15:37:50 +0100

    starting login with redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 import Customers from './components/customers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/login';
-
+import { Provider} from 'react-redux';
+import store from './store';
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         
         <Login/>
       </div>
+      </Provider>
     );
   }
 }
