7b56b24538df21dfd0f64335085dc0ffde630fc2
Rami <ramiferjani31@gmail.com>
2021-05-02 16:05:06 +0100

    added the phase feature in creating componenet

--- client/src/components/UI/Content_pages/CreateWorkout.js
+++ client/src/components/UI/Content_pages/CreateWorkout.js
@@ -1,44 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 import Exercice from "./Exercice";
+import Day from "./Day";
+function CreateWorkout(props) {
+  const [duration, setDuration] = useState("");
+  const [phase, setPhase] = useState(1);
+  const [WorkoutName, setWorkoutName] = useState("");
+  const [Paragraph, setParagraph] = useState("");
+  let numOfDays = parseInt(duration);
+  console.log(numOfDays);
+  let days = [];
+  for (let i = 0; i < numOfDays; i++) {
+    days[i] = i + 1;
+  }
+  console.log(days);
+  useEffect(() => {
+    console.log("im running");
+  });
 
-function CreateWorkout (props)  {
-  return (
-    
+  if (phase == 1) {
+    return (
       <div>
-    <Form>
-      <FormGroup>
-        <Label for="Workout">Workout Name</Label>
-        <Input
-          type="email"
-          name="email"
-          id="exampleEmail"
-        />
-        
-        
-      </FormGroup>
-      <smWorkout/>
-      <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-        <Input type="text" name="duration" id="duration" placeholder="Duration of the workout"/>
-        
-      
-      </FormGroup>
-     
-      <FormGroup>
-        <Label for="exampleText">Workout description</Label>
-        <Input type="textarea" name="text" id="exampleText" />
-      </FormGroup>
-      
+        <h3>Phase 1</h3>
+        <Form>
+          <FormGroup>
+            <Label for="Workout">Workout Name</Label>
+            <Input
+              type="text"
+              name="text"
+              id="name"
+              onChange={(event) => {
+                setWorkoutName(event.target.value);
+              }}
+              value={WorkoutName}
+            />
+          </FormGroup>
 
-      
-      <Exercice/>
-      <Button>Submit</Button>
-    </Form>
-   
-   
-    </div>
-  );
-};
+          <FormGroup>
+            <Label for="exampleSelect">Duration</Label>
+            <input
+              class="form-control"
+              type="text"
+              name="duration"
+              id="duration"
+              onChange={(event) => setDuration(event.target.value)}
+              value={duration}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="exampleText">Workout description</Label>
+            <Input
+              type="textarea"
+              name="text"
+              id="exampleText"
+              onChange={(event) => {
+                setParagraph(event.target.value);
+              }}
+              value={Paragraph}
+            />
+          </FormGroup>
+
+          <Button onClick={() => setPhase(2)}>Next</Button>
+        </Form>
+      </div>
+    );
+  } else if (phase == 2) {
+    return (
+      <div>
+        <h3>Phase 2</h3>
+        {days.map((day) => {
+          return <Day day={day} />;
+        })}
+        <Button onClick={() => setPhase(1)}>Back</Button>
+      </div>
+    );
+  }
+  console.log(days);
+}
 
 export default CreateWorkout;
