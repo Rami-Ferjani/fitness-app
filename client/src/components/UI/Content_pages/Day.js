ee4a951e5ae1788a6c0049b9842fb4a4104e3224
Rami <ramiferjani31@gmail.com>
2021-05-04 18:32:14 +0100

    day state affects the exercice state

--- client/src/components/UI/Content_pages/Day.js
+++ client/src/components/UI/Content_pages/Day.js
@@ -4,27 +4,36 @@ import Exercice from "./Exercice";
 const Day = (props) => {
   const [day, setDay] = useState(props.day);
   const [NumEx, setNumEx] = useState(0);
-
+  const [Edit,setEdit]=useState(false);
   let Exercices = [];
 
   for (let i = 0; i < NumEx; i++) {
     Exercices[i] = i + 1;
+  }
+  let buttons=<div></div>;
+  if(!Edit){
+    buttons =<div><Button
+    size="sm"
+    onClick={() => {
+      setNumEx(NumEx + 1);
+    }}
+  >
+    Add
+  </Button>
+  <Button color="info" onClick={()=>setEdit(true)}>Save</Button></div>
+  }
+  else{
+    buttons=<Button onClick={()=>setEdit(false)}>Edit</Button>
   }
   return (
     <div>
       <p> day {day}</p>
       <p>{NumEx}</p>
       {Exercices.map((exercice) => {
-        return <Exercice />;
+        return <Exercice edit={Edit} />;
       })}
-      <Button
-        size="sm"
-        onClick={() => {
-          setNumEx(NumEx + 1);
-        }}
-      >
-        Add
-      </Button>
+      {buttons}
+      
     </div>
   );
 };
