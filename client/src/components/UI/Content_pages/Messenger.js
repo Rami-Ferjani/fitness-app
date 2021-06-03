b3871a457a7d62d522ab75ed3b6a5883f138ee34
Rami <ramiferjani31@gmail.com>
2021-06-03 18:04:41 +0100

    sending a message works

--- client/src/components/UI/Content_pages/Messenger.js
+++ client/src/components/UI/Content_pages/Messenger.js
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../../../css/Messenger.css";
 import ChatOnline from "../ChatOnline";
 import Conversation from "../Conversation";
 import Message from "../Message";
 import { useSelector } from "react-redux";
 import axios from "axios";
+
 function Messenger() {
   const state = useSelector((state) => state);
   const [Conversations, setConversations] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
   const id = state.auth.person.id;
   const User = state.auth.person;
-  
+  const scrollRef = useRef();
   useEffect(() => {
     const getConversations = () => {
       axios
@@ -25,44 +27,83 @@ function Messenger() {
         });
     };
     getConversations();
-    
   }, [id]);
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get("/api/messages/" + currentChat._id);
+        setMessages(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMessages();
+  }, [currentChat]);
+  const handleSubmit = async (e) => {
+    console.log("ive started working");
+    e.preventDefault();
+    const message = {
+      sender: User.id,
+      text: newMessage,
+      conversationId: currentChat._id,
+    };
+
+    try {
+      const res = await axios.post("/api/messages", message);
+      setMessages([...messages, res.data]);
+      setNewMessage("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (scrollRef!="undefined") {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
   return (
     <div className="messenger">
       <div className="chatMenu">
         <div className="chatMenuWrapper">
           <input placeholder="search for friends" className="chatMenuUnput" />
-          {Conversations.map((conversation) => {
-            return <Conversation conversation={conversation} currentUser={User} />;
-          })}
+          {Conversations.map((conversation) => (
+            <div onClick={() => setCurrentChat(conversation)}>
+              <Conversation conversation={conversation} currentUser={User} />
+            </div>
+          ))}
         </div>
       </div>
       <div className="chatBox">
         <div className="chatBoxWrapper">
-          {
-            currentChat?
-          <>
-          <div className="chatBoxTop">
-            <Message />
-            <Message own={true} />
-            <Message />
-            <Message />
-            <Message own={true} />
-            <Message />
-            <Message />
-            <Message own={true} />
-            <Message />
-            <Message />
-            <Message own={true} />
-            <Message />
-          </div>
-          <div className="chatBoxBottom">
-            <textarea
-              className="chatMessageInput"
-              placeholder="write something..."
-            ></textarea>
-            <button className="chatSubmitButton">Send</button>
-          </div></> : <span className="noConversationText">Open a conversation  to start a chat</span>}
+          {currentChat ? (
+            <div>
+              <div className="chatBoxTop">
+                {messages.map((message) => {
+                  return (
+                    <div ref={scrollRef}>
+                      <Message message={message} own={message.sender === id} />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="chatBoxBottom">
+                <textarea
+                  className="chatMessageInput"
+                  placeholder="write something..."
+                  onChange={(event) => setNewMessage(event.target.value)}
+                  value={newMessage}
+                ></textarea>
+                <button className="chatSubmitButton" onClick={handleSubmit}>
+                  Send
+                </button>
+              </div>
+            </div>
+          ) : (
+            <span className="noConversationText">
+              Open a conversation to start a chat
+            </span>
+          )}
         </div>
       </div>
       <div className="chatOnline">
--- client/src/components/UI/Message.js
+++ client/src/components/UI/Message.js
@@ -1,7 +1,7 @@
 import "../../css/Message.css";
 import React from "react";
-
-function Message({own}) {
+import { format } from "timeago.js";
+function Message({ message, own }) {
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
@@ -10,9 +10,9 @@ function Message({own}) {
           src="https://www.w3schools.com/howto/img_avatar.png"
           alt=""
         />
-        <p className="messageText">Hello this is a message</p>
+        <p className="messageText">{message.text}</p>
       </div>
-      <div className="messageBottom">1 hour ago</div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
     </div>
   );
 }
